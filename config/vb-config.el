948e64de800be051e72cea69fd7b84120bae3e34
AriT93 <arit93@yahoo.com>
2018-02-18 16:03:45 -0800

    good bye old configs

--- config/vb-config.el
+++
@@ -1,7 +0,0 @@
-(message "loading vb-config")
-;;///////////////////////VB Mode///////////////////////////
-(autoload 'visual-basic-mode "visual-basic-mode" "Visual Basic Mode." t)
-(setq auto-mode-alist (append'(("\\.\\(frm\\|bas\\|cls\\|vbs\\)$".
-				visual-basic-mode)) auto-mode-alist))
-
-(provide 'vb-config)
