686b9620c6c80d92907ee3859da31404ec18e220
Ari Turetzky <ari.turetzky@ABSORBDEVOTESIN.workdayinternal.com>
2019-01-08 15:20:29 -0800

    updates from work for ruby and lsp

--- config/ruby-config-new.el
+++ config/ruby-config-new.el
@@ -13,7 +13,7 @@
 ;; (yas/load-directory yas/snippet-dirs)
 (use-package yasnippet-snippets
    :ensure t)
-(yas/global-mode)
+(yas-global-mode)
 (global-flycheck-mode)
 ;;(require 'xslt-process)
 
@@ -42,11 +42,22 @@
   )
 
 ;; ruby-mode-hook
-(require 'ruby-block)
-(diminish ruby-block-mode)
-(require 'ruby-electric)
-(diminish ruby-electric-mode)
-(add-hook 'ruby-mode-hook
+  ;; (use-package flymake-ruby
+  ;;   :ensure t
+  ;;   )
+;; (add-hook 'ruby-mode-hook 'flymake-ruby-load)
+(use-package rspec-mode
+  :ensure t
+  :config
+  (setq rspec-use-spring-when-possible nil)
+  :after
+  (rspec-install-snippets)
+  )
+  (require 'ruby-block)
+  (diminish 'ruby-block-mode)
+  (require 'ruby-electric)
+  (diminish 'ruby-electric-mode)
+  (add-hook 'ruby-mode-hook
             (lambda()
               (add-hook 'write-file-functions
                         '(lambda()
@@ -58,9 +69,29 @@
               (set (make-local-variable 'tab-width) 2)
               (imenu-add-to-menubar "IMENU")
               (ruby-electric-mode t)
+             (eldoc-mode -1)
+  ;;           (global-eldoc-mode -1)
+;;             (lsp-ui-doc-mode -1)
+;;             (setq lsp-ui-doc-enable-eldoc nil)
               (ruby-block-mode t)
               (define-key ruby-mode-map "\M-\C-o" 'rct-complete-symbol)
               (local-set-key (kbd "<return>") 'newline-and-indent)
+  ;;            (lsp-ui-sideline-mode t)
+              (diminish 'org-mode  "")
+              (diminish 'auto-revert-mode)
+              (diminish 'yas-minor-mode)
+              (diminish 'eldoc-mode)
+              (diminish 'org-src-mode)
+              (diminish 'eclim-mode)
+              (diminish 'abbrev-mode)
+              (diminish 'ivy-mode)
+              (diminish 'global-highline-mode)
+              (diminish 'ruby-block-mode)
+              (diminish 'ruby-electric-mode)
+              (diminish "seeing-is-believing")
+              (diminish 'hs-minor-mode)
+              (diminish 'ruby-block-mode)
+              (diminish 'global-highline-mode)
               ))
 
 (add-to-list 'auto-mode-alist '("\\.html?" . web-mode))
