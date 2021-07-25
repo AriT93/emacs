92fc2dea0c08413ad112db93a863bc18d1675e41
AriT93 <arit93@gmail.com>
2021-07-25 10:35:09 -0700

    clean up from the weekend

--- config/load-path-config-new.el
+++ config/load-path-config-new.el
@@ -2,9 +2,8 @@
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
 (add-to-list 'load-path (expand-file-name "~/emacs/site/color-theme/themes"))
 (add-to-list 'load-path (expand-file-name "~/emacs/site/lisp"))
+(add-to-list 'load-path (expand-file-name "~/emacs/site/ruby-block"))
 (add-to-list 'load-path (expand-file-name "~/emacs/site/blog"))
-(add-to-list 'load-path (expand-file-name "~/emacs/site/rcodeools"))
-(add-to-list 'load-path (expand-file-name "~/emacs/site/rdebug"))
 
 (load "ps-print")
 ;;(load-file "~/emacs/site/cedet/common/cedet.elc")
