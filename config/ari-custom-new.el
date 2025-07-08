8a734e2dff0f6dda78716af939006a205bdd5ea3
Ari Turetzky <ari.turetzky@workday.com>
2025-07-07 17:03:19 -0700

    unfill paras to make exporting from org to other places more convenient

--- config/ari-custom-new.el
+++ config/ari-custom-new.el
@@ -244,5 +244,19 @@ WEEK-OFFSET is the number of weeks before current (0 = current week)."
       
       (switch-to-buffer recap-buffer))))
 
+(defun unfill-paragraph ()
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+
+(defun unfill-region (beg end)
+  "Unfill the region, joining text paragraphs into a single
+    logical line.  This is useful, e.g., for use with
+    `visual-line-mode'."
+  (interactive "*r")
+  (let ((fill-column (point-max)))
+    (fill-region beg end)))
+
 (provide 'ari-custom-new)
 ;;; ari-custom-new.el ends here
